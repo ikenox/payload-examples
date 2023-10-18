53bc1ebbf8954f096ca6211e29dfe9f47fa2eeab
naoto.ikeno <ikenox@gmail.com>
2023-10-18 21:42:53 +0900

    example: does not work with stitched graphql schema

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,42 @@
-import express from 'express'
-import payload from 'payload'
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { stitchSchemas } from "@graphql-tools/stitch";
+import express from "express";
+import payload from "payload";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServer } from "@apollo/server";
 
-require('dotenv').config()
-const app = express()
+require("dotenv").config();
+const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
 
 const start = async () => {
-  // Initialize Payload
-  await payload.init({
+  const payloadInstance = await payload.init({
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
-  // Add your own express routes here
+  const schema1 = makeExecutableSchema({
+    resolvers: { Query: { hello: () => "Hello world!" } },
+    typeDefs: `type Query { hello: String }`,
+  });
+  const schema2 = payloadInstance.schema;
 
-  app.listen(3000)
-}
+  const stitchedSchema = stitchSchemas({
+    subschemas: [schema1, schema2],
+  });
+  const server = new ApolloServer({ schema: stitchedSchema });
+  await server.start();
 
-start()
+  app.use("/merged-graphql", express.json(), expressMiddleware(server));
+
+  app.listen(3000);
+};
+
+start();
