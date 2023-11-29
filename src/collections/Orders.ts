2351010bc4cd9a8cb21535e257fdac5f94c52de6
naoto.ikeno <ikenox@gmail.com>
2023-11-29 13:53:34 +0900

    update

---
+++ src/collections/Orders.ts
@@ -0,0 +1,18 @@
+import { CollectionConfig } from "payload/types";
+
+export const Orders: CollectionConfig = {
+  slug: "orders",
+  fields: [
+    {
+      name: "total",
+      type: "number",
+      required: true,
+    },
+    {
+      name: "selectValue",
+      type: "select",
+      hasMany: true,
+      options: ["a", "b", "c"],
+    },
+  ],
+};
